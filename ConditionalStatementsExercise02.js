bcb380eb33de25066a37c7a36e3d07bca131bd4a
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2023-10-05 21:50:18 +0300

    Update ConditionalStatementsExercise02.js

---
+++ ConditionalStatementsExercise02.js
@@ -0,0 +1,23 @@
+function bonuss(input) {
+
+    let number = Number(input[0]);
+    let bonus = 0.0;
+
+    if (number <= 100) {
+        bonus = 5;
+    } else if (number <= 1000) {
+        bonus = 0.20 * number;
+    } else {
+        bonus = 0.10 * number;
+    }
+
+    if (number % 2 === 0) {
+        bonus = bonus + 1;
+    } else if (number % 10 === 5) {
+        bonus = bonus + 2;
+    }
+
+    console.log(bonus)
+    console.log(bonus + number)
+
+}
