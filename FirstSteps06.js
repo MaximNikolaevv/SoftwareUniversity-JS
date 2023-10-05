1324baa84a9458dc4cfd4938e15cb522a0fb3a59
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2023-10-05 21:49:58 +0300

    Update FirstSteps06.js

---
+++ FirstSteps06.js
@@ -0,0 +1,10 @@
+function solve(input) {
+
+    let firstName = input[0]
+    let lastname = input[1]
+    let age = Number(input[2])
+    let town = input[3]
+
+    console.log(`You are ${firstName} ${lastname}, a ${age}-years old person from ${town}.`)
+
+}
